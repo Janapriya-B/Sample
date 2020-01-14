35360f2e68eeb876826d5954e59d2212a21df020
janapriya <janapriya@Janapriya-PC>
2020-01-14 13:28:08 +0530

    Changes committed on 14 jan

---
+++ Sampleprograms/src/oddeven.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class oddeven
+{
+  public static void main(String args[])
+  {
+    int x;
+    System.out.println("Enter an integer to check if it's odd or even");
+    Scanner a = new Scanner(System.in);
+    x = a.nextInt();
+
+    if (x % 2 == 0)
+      System.out.println("The number is even.");
+    else
+      System.out.println("The number is odd.");
+  }
+}
